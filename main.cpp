cedbd90f8667ce2c1de3b247d362863c10c47b04
Nathan Hansen <nate1hansen@gmail.com>
2012-03-07 09:57:16 -0700

    Initial Creation of project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Nathan Hansen
+ *
+ * Created on March 7, 2012, 9:44 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
